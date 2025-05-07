7e7ee65de0976323b85573b5b5d76ac596a7f874
4tKnight <osagiepromise79@gmail.com>
2025-05-07 10:12:01 +0100

    fix

--- index.js
+++ index.js
@@ -23,3 +23,7 @@ const PORT = process.env.PORT || 4000
 app.listen(PORT, ()=>{
     console.log(`Server running on port ${PORT}`);
 })
+
+// PORT=9000
+// MONGO_URL=mongodb+srv://user:password12345.@cluster0.kjkbhqn.mongodb.net/
+// CORS_DOMAINS=http://localhost:9000,https://localhost:9000,https://live-server-dio0.onrender.com,http://live-server-dio0.onrender.com
